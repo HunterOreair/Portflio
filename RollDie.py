c94474c6d9b2305e4d6659dc0e349f1c540f78f4
HunterOreair <46851469+HunterOreair@users.noreply.github.com>
2019-01-19 15:42:01 -0500

    Add files via upload

---
+++ RollDie.py
@@ -0,0 +1,23 @@
+from random import randint # imports randint. duh.
+
+min = 1 #sets the minimum value on the dice that you can roll
+max = 6 #sets the maximum value
+
+def die(): # Creates a method called die
+    rollDie = randint(min, max) # sets what number you rolled
+    print("You rolled a", rollDie) # prints the number you rolled
+
+def rollAgain():
+    again = True
+
+    while again: #while 'again' is True, do the following:
+        die() # calls the die method.
+        print("Would you like to roll again? y/n") #asks if you would like to roll again.
+        ans = input() #takes your input
+
+        if ans == "y": #if your input equals 'y' then run the while loop again
+            again = True
+        else: #anything will end the program
+            again = False
+
+rollAgain() # calls rollAgain method, running the program.
